bba1172a034062ada21100d1de675acd7936e6c4
Yukinori NAKATA <yukinorinkt@gmail.com>
2025-03-07 01:12:40 +0900

    Fix .gitignore

--- obj/Debug/net9.0/CheckSumEditor.AssemblyInfo.cs
+++ obj/Debug/net9.0/CheckSumEditor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CheckSumEditor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c213c8ceeeed32f62478ebec1b73349b3ed9f7e")]
 [assembly: System.Reflection.AssemblyProductAttribute("CheckSumEditor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CheckSumEditor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
